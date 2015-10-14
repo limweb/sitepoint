3197148bf7725242da05cc119d7cc41c1cc31c62
mrlami <lami@inbox.com>
2015-10-14 15:13:30 -0400

    [master] add php csrf files - start

---
+++ php security/03-php-csrf/app/index.php
@@ -0,0 +1,56 @@
+<?php
+    $email    = null;
+    $fullname = null;
+    $password = null;
+
+    if(isset($_REQUEST['update'])) {
+        $email    = !empty($_REQUEST['email']) ? $_REQUEST['email'] : null;
+        $fullname = !empty($_REQUEST['fullname']) ? $_REQUEST['fullname'] : null;
+        $password = !empty($_REQUEST['password']) ? $_REQUEST['password'] : null;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSRF Protection in PHP | Sitepoint</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <style type="text/css">
+        body {padding: 4em;}
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="col-sm-6">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label for="email" class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="email" id="email" <?php echo ($email) ? "value='$email'" : "placeholder='Default Email'" ?>>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="fullname" class="col-sm-2 control-label">Fullname</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="fullname" id="fullname" <?php echo ($fullname) ? "value='$fullname'" : "placeholder='Default Fullname'" ?>>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-sm-2 control-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="password" id="password" <?php echo ($password) ? "value='$password'" : "placeholder='Default Password'" ?>>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-info" name="update" value="1">Update Profile</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
